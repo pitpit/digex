ba256ab970dc0266941e1954fd6eba31a0cb73e3
Damien Pitard <damien.pitard@gmail.com>
2011-12-14 10:24:00 +0100

    updated to Digex #c55a905

--- app/autoload.php
+++ app/autoload.php
@@ -6,8 +6,3 @@
  */
 
 require __DIR__.'/../vendor/.composer/autoload.php';
-
-//Uncomment the following lines to developp on Digex-Core
-//$loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
-//$loader->registerNamespace('Digex', __DIR__.'/../vendor/digex/src');
-//$loader->register();
--- app/data/schema.sql
+++
@@ -1,1 +0,0 @@
--- initial database schema
