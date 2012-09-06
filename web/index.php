b1093dac5c62f5147f24fb9b7305baeaa478a340
Damien Pitard <damien.pitard@gmail.com>
2012-09-06 18:22:20 +0200

    moved autoload.php (according to last composer version)

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@
 // This will let the permissions be 0777
 //umask(0000); 
 
-require_once __DIR__ . '/../app/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../app/Application.php';
 
 $app = new Application(new Silex\Application(), 'prod', false);
--- web/index_dev.php
+++ web/index_dev.php
@@ -11,7 +11,7 @@ error_reporting(E_ALL);
 // This will let the permissions be 0777
 //umask(0000); 
 
-require_once __DIR__ . '/../app/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../app/Application.php';
 
 $app = new Application(new Silex\Application(), 'dev', true);
