1e16a50bccff5557ec7aa65555a16e6a6a77908d
Damien Pitard <damien.pitard@gmail.com>
2012-09-11 15:20:33 +0200

    updated to last dcc-core (6f5c38ef7afac694948db2e1e0ec8a4482db16d5)

--- web/index_dev.php
+++ web/index_dev.php
@@ -1,11 +1,12 @@
 <?php
 
 //umask(0000);  //This will let the permissions be 0777
-
 ini_set('display_errors', 1); 
 error_reporting(E_ALL);
 
+require_once __DIR__.'/../vendor/autoload.php';
+
+$env = 'dev'; //pass the current environment to app.php
 $app = require __DIR__.'/../app/app.php';
 $app['debug'] = true;
-$app['env'] = 'dev';
 $app->run();
